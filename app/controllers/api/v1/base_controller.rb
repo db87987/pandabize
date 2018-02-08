8e3f2a2a95979ebff1ce7dcf64f77b51d165b614
Dragon Black <db87987@gmail.com>
2018-02-08 17:30:43 +0300

    draft product page

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,3 +1,4 @@
 class Api::V1::BaseController < ApplicationController
+  include ExceptionHandler
   respond_to :json
 end
--- app/javascript/components/Product.js
+++ app/javascript/components/Product.js
@@ -1,10 +1,37 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { Redirect } from 'react-router-dom'
+
 class Product extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { product: {}, redirectToHome: false };
+  }
+
+  componentDidMount() {
+    fetch(`/api/v1/products/${this.props.match.params.id}`)
+    .then(response => {
+      if (!response.ok) { throw response }
+      return response.json();
+    }).then(data => {
+      this.setState({ product: data })
+    })
+    .catch(error => {
+      this.setState({ redirectToHome: true })
+    })
+  }
+
   render () {
+    if (this.state.redirectToHome) {
+      return (
+        <Redirect to="/"/>
+      )
+    }
+
     return (
       <div>
-        Product page
+        <h3>{ this.state.product.name }</h3>
+        <div>{ this.state.product.description }</div>
       </div>
     );
   }
