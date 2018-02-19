e0aa87e8f6f2f00ba27b97472288eadc9e3dc51a
Dragon Black <db87987@gmail.com>
2018-02-19 11:43:49 +0300

    exclude options from product serializer

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,15 +1,5 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :options
+  attributes :id, :name, :description
 
   has_many :variants
-
-  def options
-    self.object.option_types.includes(:values).map do |option_type|
-      {
-        id: option_type.id,
-        name: option_type.name,
-        values: option_type.values.map { |value| OptionValueSerializer.new(value) }
-      }
-    end
-  end
 end
